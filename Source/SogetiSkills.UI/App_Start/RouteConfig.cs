c75601d8ced715d994ccc7d84df75631d903c41a
John Wathen <jswathen@asu.edu>
2014-08-31 11:41:00 -0700

    Added layout with bootstrap.

--- Source/SogetiSkills.UI/App_Start/RouteConfig.cs
+++ Source/SogetiSkills.UI/App_Start/RouteConfig.cs
@@ -1,23 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 using System.Web.Routing;
+using AttributeRouting.Web.Mvc;
+using SogetiSkills.UI.Controllers;
 
-namespace SogetiSkills.UI
+namespace SogetiSkills.UI 
 {
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+    public static class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes) 
+		{
+            routes.MapAttributeRoutes(config =>
+                {
+                    config.AddRoutesFromAssemblyOf<HomeController>();
+                    config.AppendTrailingSlash = true;
+                    config.UseLowercaseRoutes = true;
+                });
+		}
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+        public static void Start() 
+		{
+            RegisterRoutes(RouteTable.Routes);
         }
     }
 }
---
+++ Source/SogetiSkills.UI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using AttributeRouting.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SogetiSkills.UI.Controllers
+{
+    public partial class HomeController : Controller
+    {
+        [GET("/")]
+        public virtual ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
