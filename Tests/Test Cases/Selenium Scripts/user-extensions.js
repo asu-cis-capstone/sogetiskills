0b8022d34e986f2506730c210ba7a3a3187bd97e
john monje <jsmonje@gmail.com>
2014-10-26 18:02:07 -0700

    tests

--- Tests/Test Cases/Selenium Scripts/user-extensions.js
+++ Tests/Test Cases/Selenium Scripts/user-extensions.js
@@ -10,45 +10,86 @@
 *
 * You can find contributed extensions at http://wiki.openqa.org/display/SEL/Contributed%20User-Extensions
 */
-
 // The following examples try to give an indication of how Selenium can be extended with javascript.
-
 // All do* methods on the Selenium prototype are added as actions.
-// E.g.add a typeRepeated action to Selenium, which types the text twice into a text box.
+// Eg add a typeRepeated action to Selenium, which types the text twice into a text box.
 // The typeTwiceAndWait command will be available automatically
-
-
+Selenium.prototype.doTypeRepeated = function(locator, text) {
+// All locator-strategies are automatically handled by "findElement"
+var element = this.page().findElement(locator);
 // Create the text to type
-
-
+var valueToType = text + text;
+// Replace the element text with the new text
+this.page().replaceText(element, valueToType);
+};
 // All assert* methods on the Selenium prototype are added as checks.
 // Eg add a assertValueRepeated check, that makes sure that the element value
 // consists of the supplied text repeated.
 // The verify version will be available automatically.
-
-
-
+Selenium.prototype.assertValueRepeated = function(locator, text) {
+// All locator-strategies are automatically handled by "findElement"
+var element = this.page().findElement(locator);
+// Create the text to verify
+var expectedValue = text + text;
+// Get the actual element value
+var actualValue = element.value;
+// Make sure the actual value matches the expected
+Assert.matches(expectedValue, actualValue);
+};
 // All get* methods on the Selenium prototype result in
 // store, assert, assertNot, verify, verifyNot, waitFor, and waitForNot commands.
 // E.g. add a getTextLength method that returns the length of the text
 // of a specified element.
 // Will result in support for storeTextLength, assertTextLength, etc.
 
+/* <tr>
+	<td>store</td>
+	<td>javascript{(new Date().getTime())}</td>
+	<td>datetime</td>
+</tr> */
+
+Selenium.prototype.doFillEmail = function(locator){
+    var time = new Date(); 
+    var time = time.getTime();
+    var email = time + '@gmail.com' ;
+    this.doType(locator, email);
+}
+
+Selenium.prototype.doFillPassword = function(locator){
+   var time = new Date(); 
+    var time = time.getTime();
+    var email = 'password11' ;
+    this.doType(locator, email);
+}
 
 
+
+/* 
+Selenium.prototype.doTypePswd = function(locator){
+    var pswd = new pswd(); 
+    var pswd = 'password11';
+	this.doType(locator, pswd);
+}
+
+ */
+
+
+Selenium.prototype.getTextLength = function(locator) {
+return this.getText(locator).length;
+};
 // All locateElementBy* methods are added as locator-strategies.
 // Eg add a "valuerepeated=" locator, that finds the first element with the supplied value, repeated.
 // The "inDocument" is a the document you are searching.
-
-Selenium.prototype.doLogin = function(value,varName){
-var paraPart= [];
-paraPart =value.match(/(.*)\/(.*)/);
-var LOGON = paraPart[1];
-var PASSWORD = paraPart[2];
-page =selenium.browserbot.getCurrentWindow();
-page.document.getElementById("Login1_UserName").value=LOGON;
-page.document.getElementById("Login1_Password").value=PASSWORD;
-page.document.getElementsByName("Login1$LoginButton").click();
-
-return;
-} 
+PageBot.prototype.locateElementByValueRepeated = function(text, inDocument) {
+// Create the text to search for
+var expectedValue = text + text;
+// Loop through all elements, looking for ones that have a value === our expected value
+var allElements = inDocument.getElementsByTagName("*");
+for (var i = 0; i < allElements.length; i++) {
+var testElement = allElements[i];
+if (testElement.value && testElement.value === expectedValue) {
+return testElement;
+}
+}
+return null;
+};
